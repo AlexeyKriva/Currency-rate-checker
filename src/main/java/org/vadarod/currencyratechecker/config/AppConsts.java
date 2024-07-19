2a2b719806f91e7819f3a84a19c656df5d4d5411
AlexeyKriva <alexey.kriva03.com@gmail.com>
2024-07-19 09:29:19 +0300

    Add date service and application constants

---
+++ src/main/java/org/vadarod/currencyratechecker/config/AppConsts.java
@@ -0,0 +1,17 @@
+package org.vadarod.currencyratechecker.config;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Year;
+
+public class AppConsts {
+    public static final int START_YEAR = 2000;
+    public static final int START_MONTH = 1;
+    public static final int START_DAY = 1;
+    public static final int END_YEAR = LocalDate.now().getYear();
+    public static final int END_MONTH = LocalDate.now().getMonthValue();
+    public static final int END_DAY = LocalDate.now().getDayOfMonth();
+    public static final int NUMBER_OF_MONTHS = Month.values().length;
+    public static final String DATE_PATTERN = "yyyy-M-d";
+    public static final int PERIODICITY = 0;
+}
---
+++ src/main/java/org/vadarod/currencyratechecker/services/DateService.java
@@ -0,0 +1,26 @@
+package org.vadarod.currencyratechecker.services;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static org.vadarod.currencyratechecker.config.AppConsts.*;
+import static org.vadarod.currencyratechecker.config.AppConsts.END_DAY;
+
+@Service
+public class DateService {
+    public static String formatDate(int year, int month, int day) {
+        LocalDate date = LocalDate.of(year, month, day);
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(DATE_PATTERN);
+        return date.format(dateFormatter);
+    }
+
+    public static boolean hasMonthPassed(int year, int month) {
+        return year == END_YEAR && month > END_MONTH;
+    }
+
+    public static boolean hasDayPassed(int year, int month, int day) {
+        return year == END_YEAR && month == END_MONTH && day > END_DAY;
+    }
+}
