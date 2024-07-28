4d106fb62e40bfe92300a1a59dea7d8af0efc577
AlexeyKriva <alexey.kriva03.com@gmail.com>
2024-07-28 22:36:53 +0300

    Add exception handler

--- src/main/java/org/vadarod/currencyratechecker/controllers/CurrencyController.java
+++ src/main/java/org/vadarod/currencyratechecker/controllers/CurrencyController.java
@@ -1,7 +1,9 @@
 package org.vadarod.currencyratechecker.controllers;
 
+import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import org.vadarod.currencyratechecker.entities.CurrencyDto;
@@ -9,17 +11,24 @@ import org.vadarod.currencyratechecker.services.CurrencyService;
 
 @RestController
 @RequestMapping(value = "/api", produces = "application/json")
+@Validated
 public class CurrencyController {
     @Autowired
     private CurrencyService currencyService;
 
     @PostMapping("/load-currency-rates/{ondate}")
-    public ResponseEntity<String> loadCurrencyRates(@PathVariable("ondate") String date) {
+    public ResponseEntity<String> loadCurrencyRates(@PathVariable("ondate")
+            @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "The date should be in as follows yyyy-mm-dd")
+                                                        String date) {
         return currencyService.loadCurrencyRatesResponse(date);
     }
 
     @GetMapping("/get-currency-rate/{ondate}/{curId}")
-    public ResponseEntity<CurrencyDto> findCurrencyDtoRate(@PathVariable("ondate") String date,
+    public ResponseEntity<CurrencyDto> findCurrencyDtoRate(@PathVariable("ondate")
+                                                               @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$",
+                                                                       message = "The date should be in as" +
+                                                                               " follows yyyy-mm-dd")
+                                                               String date,
                                                            @PathVariable("curId") int curId) {
         return currencyService.findCurrencyDtoRateByDateAndCurIdResponse(date, curId);
     }
---
+++ src/main/java/org/vadarod/currencyratechecker/controllers/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package org.vadarod.currencyratechecker.controllers;
+
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+        exception.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Map<String, String>> handleConstraintViolationException(ConstraintViolationException exception) {
+        Map<String, String> errors = new HashMap<>();
+        exception.getConstraintViolations().forEach((violation) -> {
+            String fieldName = violation.getPropertyPath().toString();
+            String errorMessage = violation.getMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+}
